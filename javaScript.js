05052b4a33a228ded4a3cc49faf37507f8b936ac
ariomega <aristide.odrg@gmail.com>
2020-03-20 11:43:35 +0100

    dark mode edition

--- javaScript.js
+++ javaScript.js
@@ -3,3 +3,17 @@
         document.getElementById("img01").src = element.src;
         document.getElementById("modal01").style.display = "block";
     }
+
+    
+
+    function switchTheme(event) {
+        if (event.target.checked) {
+            document.documentElement.setAttribute('data-theme', 'dark');
+        }
+        else {
+            document.documentElement.setAttribute('data-theme', 'light');
+        }
+    }
+
+    const toggler = document.querySelector('#toggler');
+    toggler.addEventListener('change', switchTheme, false);
